881cf7eb443bfb465548371774c47160478277f7
Kartik <kkartiktummawar@gmail.com>
2025-01-01 12:13:19 +0530

    Final Touch

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,31 @@
+
 import { SignIn } from '@clerk/nextjs'
 import React from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
 
 const SignInPage = () => {
     return (
-        <main className='flex h-screen w-full items-center justify-center'>
+        <main className='flex flex-col h-screen w-full items-center justify-center'>
+            <Dialog>
+                <DialogTrigger className='text-white bg-blue-1 rounded'>Click Here For Demo Accounts</DialogTrigger>
+                <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+                    <DialogHeader>
+                        <DialogDescription className='font-bold'>
+                            <h3>1.Username : john , Password : john@demo</h3>
+                            <h3>2.Username : hannah , Password : hannah@demo</h3>
+                            <p>Alternatively,you can create your own account. Thank You!</p>
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+
             <SignIn />
         </main>
     )

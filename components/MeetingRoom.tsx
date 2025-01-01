a7e10c5fab42ca1815b2ecf50612dfd3921cda78
Kartik <kkartiktummawar@gmail.com>
2025-01-01 11:10:04 +0530

    Personal Room - Done

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -10,7 +10,7 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { LayoutList, Users } from 'lucide-react'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import EndCallButton from './EndCallButton'
 import Loader from './Loader'
 
@@ -22,7 +22,7 @@ const MeetingRoom = () => {
     const [showParticipants, setShowParticipants] = useState(false)
     const searchParams = useSearchParams()
     const isPersonalRoom = !!searchParams.get('personal')
-
+    const router = useRouter()
     const { useCallCallingState } = useCallStateHooks()
     const callingState = useCallCallingState()
 
@@ -55,7 +55,11 @@ const MeetingRoom = () => {
             </div>
 
             <div className='fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap'>
-                <CallControls />
+                <CallControls
+                    onLeave={() => {
+                        router.push("/")
+                    }}
+                />
 
                 <DropdownMenu>
 
